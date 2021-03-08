33327f80de64fa560ce94c3905cd66e94ee49358
Dariel Vicedo <darielvicedo@gmail.com>
2021-03-07 20:42:56 -0500

    core: gh-pages deploy config

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,16 @@
 module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/nomadasxcuba/'
+    : '/',
+  pages: {
+    index: {
+      entry: 'src/main.js',
+      template: 'public/index.html',
+      filename: 'index.html',
+      title: 'Nómadas por Cuba',
+    },
+  },
   transpileDependencies: [
-    'vuetify'
+    'vuetify',
   ]
 }
