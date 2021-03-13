0b4da8295242a74b6c456fe5b8bd812f3daa4bef
Dariel Vicedo <darielvicedo@gmail.com>
2021-03-13 09:57:56 -0500

    imp: Background pattern

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <v-app id="nomadasxcuba">
     <Header></Header>
-    <v-main class="grey lighten-3">
+    <v-main>
       <router-view></router-view>
     </v-main>
   </v-app>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
 import vuetify from './plugins/vuetify';
-import router from './router'
+import router from './router';
+import './assets/css/main.scss';
 
 Vue.config.productionTip = false
 
